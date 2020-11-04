5f152acf645a22dcc01fc791cb2e89ea526d3656
Carlos Justino <carlos.justino08@gmail.com>
2020-11-04 16:36:54 -0300

    adicao da Cidade e Estado

---
+++ src/main/java/br/com/justino/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package br.com.justino.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.justino.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
