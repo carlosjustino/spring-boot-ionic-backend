6ad46ed7c875010348d6adae693ca6b5ec82a6ff
Carlos Justino <carlos.justino08@gmail.com>
2020-11-06 15:34:38 -0300

    Categoria Delete

--- src/main/java/br/com/justino/cursomc/ionic/backend/services/CategoriaService.java
+++ src/main/java/br/com/justino/cursomc/ionic/backend/services/CategoriaService.java
@@ -3,10 +3,12 @@ package br.com.justino.cursomc.ionic.backend.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.justino.cursomc.ionic.backend.domain.Categoria;
 import br.com.justino.cursomc.ionic.backend.repositories.CategoriaRepository;
+import br.com.justino.cursomc.ionic.backend.services.exceptions.DataIntegrityException;
 import br.com.justino.cursomc.ionic.backend.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -14,21 +16,32 @@ public class CategoriaService {
 
 	@Autowired
 	CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
-		
+
 		Optional<Categoria> obj = repo.findById(id);
-		
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possua referencias!", e);
+
+		}
 	}
 }
