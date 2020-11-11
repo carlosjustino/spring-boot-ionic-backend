55c8636df6a0444910de59810dae8c911e7db469
Carlos Justino <carlos.justino08@gmail.com>
2020-11-11 09:48:30 -0300

    Envio de email via smtp

--- src/main/java/br/com/justino/cursomc/ionic/backend/services/EmailService.java
+++ src/main/java/br/com/justino/cursomc/ionic/backend/services/EmailService.java
@@ -1,9 +1,11 @@
 package br.com.justino.cursomc.ionic.backend.services;
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
 import br.com.justino.cursomc.ionic.backend.domain.Pedido;
 
+@Service
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
