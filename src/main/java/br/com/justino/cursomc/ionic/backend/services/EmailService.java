b300948ecd396f5a584e9cab2bbe16db711102d0
Carlos Justino <carlos.justino08@gmail.com>
2020-11-11 10:13:49 -0300

    Email HTML

--- src/main/java/br/com/justino/cursomc/ionic/backend/services/EmailService.java
+++ src/main/java/br/com/justino/cursomc/ionic/backend/services/EmailService.java
@@ -1,5 +1,7 @@
 package br.com.justino.cursomc.ionic.backend.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +11,9 @@ import br.com.justino.cursomc.ionic.backend.domain.Pedido;
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
-	
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
